5d0be2a1ed01f5e8485b6339f6b55ec75d17ce8c
ebreton <email>
2020-01-26 19:04:14 +0100

    Add authentication_token_from_email

--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
@@ -3,10 +3,8 @@ import requests
 from app import crud
 from app.core import config
 from app.db.session import db_session
-from app.models.user import UserCreate
-from faker import Faker
-
-fake = Faker(config.LOCALE_FOR_TESTS)
+from app.schemas.user import UserCreate, UserUpdate
+from app.tests.utils.utils import get_server_api, random_lower_string
 
 
 def user_authentication_headers(server_api, email, password):
@@ -20,7 +18,6 @@ def user_authentication_headers(server_api, email, password):
 
 
 def create_random_user():
-    password = fake.password()
 
     # generate random data for user
     user_in = UserCreate(
@@ -36,3 +33,21 @@ def create_random_user():
     created = crud.user.create(db_session=db_session, user_in=user_in)
     created.password = password
     return created
+
+
+def authentication_token_from_email(email):
+    """
+    Return a valid token for the user with given email.
+
+    If the user doesn't exist it is created first.
+    """
+    password = random_lower_string()
+    user = crud.user.get_by_email(db_session, email=email)
+    if not user:
+        user_in = UserCreate(username=email, email=email, password=password)
+        user = crud.user.create(db_session=db_session, obj_in=user_in)
+    else:
+        user_in = UserUpdate(password=password)
+        user = crud.user.update(db_session, obj_in=user, db_obj=user_in)
+
+    return user_authentication_headers(get_server_api(), email, password)
